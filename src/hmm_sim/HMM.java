3beacd267524721f14b03abb71c6859cc7cc7582
Lucas Langer <lucaslangermont9@gmail.com>
2015-05-07 19:59:30 -0400

    debugging 0-1

--- src/hmm_sim/HMM.java
+++ src/hmm_sim/HMM.java
@@ -1,4 +1,4 @@
-//HMM model written by Lucas Langer to better one's intuition about EM
+//HMM model written by Lucas Langer to better my intuition about EM
 
 package hmm_sim;
 
@@ -13,21 +13,38 @@ public class HMM {
 	private Matrix O;
 	private Matrix P;
 	
-	private Random random;
+	public Random random;
 	
+	public static void main(String[] args){
+		double[][] t = { {0.6,0.3,0.1}, {0.3,0.3,0.4}, {0.1,0.7,0.2} }; 
+		double[][] o = { {0.6,0.3,0.1}, {0.3,0.3,0.4}, {0.1,0.7,0.2} };  
+		double[][] p = { {0.3}, {0.4}, {0.4} };
+		
+		Matrix T = new Matrix( t );
+		Matrix O = new Matrix( o );
+		Matrix P = new Matrix( p );
+		HMM h = new HMM(T,O,P);
+		
+		double[] sequence = h.generateSequence(100);
+		System.out.println( Arrays.toString(sequence) );
+		Set<String> s = h.expectationMaximizationGaussian(sequence, 3 , 5);
+		System.out.println( s.toString() );
+	}
+
 	public HMM(Matrix T, Matrix P, Matrix O){
 		this.T = T;
 		this.O = O;
 		this.P = P;
+		
+		random = new Random();
 	}
 	
 	public double[] generateSequence(int duration){
 		double[] oSeq = new double[duration];
 		
-		int hiddenState = generateState(this.P.getArray()[0]);		
-		int t = 0;
+		int hiddenState = generateState( P.getArray()[0] );		
 		
-		while (t < duration){
+		for(int t=0;t<duration;t++){
 			hiddenState = generateState( T.getArray()[hiddenState] );
 			oSeq[t] = generateState( O.getArray()[hiddenState] );
 		} 
@@ -38,16 +55,17 @@ public class HMM {
 	//[0.4, 0.5, 0.1] --> returns index in {0,1,2}
 	public int generateState(double[] stateProbabilities){ 						
 		int l = stateProbabilities.length; 
-		double[] cumulativeSum = new double[l-1];
+		double[] cumulativeSum = new double[l];
 		
 		cumulativeSum[0] = stateProbabilities[0];
-		for (int i = 1; i<l-1;i++){
+		for (int i = 1; i<l;i++){
 			cumulativeSum[i] = cumulativeSum[i-1] + stateProbabilities[i];
 		}
-		
+
 		double r = random.nextDouble();
 		
 		int index = Arrays.binarySearch(cumulativeSum, r);
+
 		if (index >= 0){
 			return index;
 		}
@@ -57,49 +75,73 @@ public class HMM {
 		
 	}	
 	
-	
 	//Move this to another class?
-	public Matrix expectationMaximizationGaussian(double[] observationSequence, int clusters, int numIterations){
+	public Set<String> expectationMaximizationGaussian(double[] observationSequence, int clusters, int numIterations){
 		double overallMean = getMean(observationSequence);
 		double overallSD = getSd(observationSequence, overallMean);
 		
 		HashMap<String, ArrayList<Double>> emData = new HashMap<String, ArrayList<Double>>();
 		
+		double meanGuess, sdGuess;
 		for(int count = 0;count < clusters;count++){
-			double meanGuess = overallMean - overallSD + random.nextDouble()*2*overallSD;
-			double sdGuess = overallSD/clusters;
+		    meanGuess = overallMean - overallSD + random.nextDouble()*2*overallSD;
+			sdGuess = overallSD/clusters;
 			
 			String key = Double.toString(meanGuess) + "," + Double.toString(sdGuess);
-			emData.put(key, new ArrayList<Double>() );  	
+			emData.put(key, new ArrayList<Double>() ); 
 		}
 		
-		int iteration = 0;
-		while (iteration < numIterations){
-			
-			emData = maximize(emData, observationSequence);
-			expect(emData);
-			
+		for(int iteration=0;iteration<numIterations;iteration++){
+			expect(emData, observationSequence);
+			maximize(emData);
 		}
 		
-		
+		return emData.keySet();
 	}
 	
-	public HashMap<String, ArrayList<Double>> expect(HashMap<String, ArrayList<Double>> emData, double[] observationSequence){
+	public void expect(HashMap<String, ArrayList<Double>> emData, double[] observationSequence){
+		String maxKey = null;
+		double maxProbability = 0;
 		
+		double mean, sd, likelyhood;
+		ArrayList<Double> currentData;
+		for(double datapoint: observationSequence){
+			for(String key: emData.keySet()){
+				mean = Double.parseDouble(key.split(",")[0]);
+				sd = Double.parseDouble(key.split(",")[1]);
+				likelyhood = getLikelyhood(mean, sd, datapoint);
+				if (maxKey == null || likelyhood > maxProbability ){
+					maxKey = key;
+					maxProbability = likelyhood;
+				}
+			}
+			currentData = emData.get(maxKey);
+			currentData.add(datapoint);
+			maxProbability = 0;
+			maxKey = null;
+		}
 	}
 	
-	public HashMap<String, ArrayList<Double>> maximize(HashMap<String, ArrayList<Double>> emData, double[] observationSequence){
+	public void maximize(HashMap<String, ArrayList<Double>> emData){
 		
+		//Reason for copy is to avoid overwriting 
 		HashMap<String, ArrayList<Double>> revisedEmData = new HashMap<String, ArrayList<Double>>();
+		double[] l;
+		double mean, sd;
 		for (String key: emData.keySet()){
-			double[] l = listToArray( emData.get(key));
-			double mean = getMean(l);
-			double sd = getSd(l, mean);
+			l = listToArray( emData.get(key));
+			mean = getMean(l);
+			sd = getSd(l, mean);
 			String newkey = Double.toString(mean) + "," + Double.toString(sd);
 			revisedEmData.put(newkey, new ArrayList<Double>());
 		}
-		
-		return revisedEmData;
+		emData = revisedEmData;
+	}
+	
+	public double getLikelyhood(double modelMean, double modelSd, double obs){
+		double num = Math.pow(Math.E, -1*Math.pow(modelMean - obs,2) / (2*Math.pow(modelSd,2)) );
+		double denom = Math.sqrt(2*Math.pow(modelSd,2)*Math.PI);
+		return num/denom;
 	}
 	
 	public static double[] listToArray(List<Double> arr){   
@@ -127,11 +169,4 @@ public class HMM {
 		}
 		return Math.sqrt(m/da.length);
 	}
-	
-	public double getLikelyhood(double modelMean, double modelSd, double obs){
-		double num = Math.pow(Math.E, -1*Math.pow(modelMean - obs,2) / (2*Math.pow(modelSd,2)) );
-		double denom = Math.sqrt(2*Math.pow(modelSd,2)*Math.PI);
-		return num/denom;
-	}
-
 }
