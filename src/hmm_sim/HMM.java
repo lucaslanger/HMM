1ae1abb3c839cef1dab71bc220b8aba1d573fa2c
Lucas Langer <lucaslangermont9@gmail.com>
2015-05-11 14:58:59 -0400

    Both simulations working

--- src/hmm_sim/HMM.java
+++ src/hmm_sim/HMM.java
@@ -3,7 +3,6 @@
 package hmm_sim;
 
 import Jama.*;
-
 import java.util.*;
 
 public class HMM {
@@ -22,9 +21,11 @@ public class HMM {
 		double c2 = random.nextDouble();
 		double c3 = random.nextDouble();
 		
+		/*
 		System.out.println(c1);
 		System.out.println(c2);
 		System.out.println(c3);
+		*/
 		
 		double[][] t = { {0.3,0.3,0.4}, {0.3,0.3,0.4}, {0.3,0.3,0.4} }; 
 		double[][] o = { generateProbabilityVector(10,c1), 
@@ -41,44 +42,14 @@ public class HMM {
 		
 		double[] sequence = h.generateSequence(100);
 		Set<String> s = h.expectationMaximizationGaussian(sequence, 3 , 50);
-		System.out.println(s);
 		
-		//testHankel();
+		testHankel();
 	}
 
 	public HMM(Matrix T, Matrix O, Matrix P){
 		this.T = T;
 		this.O = O;
 		this.P = P;
-	}
-	
-	public static double[] generateProbabilityVector(int length, double p){	
-		double[] result = new double[length]; 
-		for(int i=0;i<length;i++){
-			result[i] = nChooseI(length-1, i)*Math.pow(p,i)*Math.pow(1-p, length-1-i);
-		} 
-	
-		return result;
-	}
-	
-	public static double sumArray(double[] da){
-		double s = 0;
-		for(double d: da){
-			s += d;
-		}
-		return s;
-	}
-	
-	public static int nChooseI(int n, int i){
-		return factorial(n)/ ( factorial(i)*factorial(n-i) );
-	}
-	public static int factorial(int n){
-		if (n == 0){
-			return 1;
-		}
-		else{
-			return n*factorial(n-1);
-		}
 	}
 	
 	public double[] generateSequence(int duration){
@@ -94,30 +65,6 @@ public class HMM {
 		return oSeq;
 	}
 
-	//[0.4, 0.5, 0.1] --> returns index in {0,1,2}
-	public static int generateState(double[] stateProbabilities){ 						
-		int l = stateProbabilities.length; 
-		double[] cumulativeSum = new double[l];
-		
-		cumulativeSum[0] = stateProbabilities[0];
-		for (int i = 1; i<l;i++){
-			cumulativeSum[i] = cumulativeSum[i-1] + stateProbabilities[i];
-		}
-
-		double r = random.nextDouble();
-		
-		int index = Arrays.binarySearch(cumulativeSum, r);
-
-		if (index >= 0){
-			return index;
-		}
-		else{
-			return -1*(index + 1);
-		}
-		
-	}	
-	
-	//Move this to another class?
 	public Set<String> expectationMaximizationGaussian(double[] observationSequence, int clusters, int numIterations){
 		double overallMean = getMean(observationSequence);
 		double overallSD = getSd(observationSequence, overallMean);
@@ -136,10 +83,8 @@ public class HMM {
 		for(int iteration=0;iteration<numIterations;iteration++){
 			
 			expect(emData, observationSequence);
-			System.out.println(emData.values());
-			//WHY is emData not mutable under maximize?
+			//Investigate: WHY is emData not mutable under maximize?
 			emData = maximize(emData);
-			System.out.println(emData.keySet());
 		}
 		
 		return emData.keySet();
@@ -192,51 +137,6 @@ public class HMM {
 		return revisedEmData;
 	}
 	
-	public double getLikelyhood(double modelMean, double modelSd, double obs){
-		double num = Math.pow(Math.E, -1*Math.pow(modelMean - obs,2) / (2*Math.pow(modelSd,2)) );
-		double denom = Math.sqrt(2*Math.pow(modelSd,2)*Math.PI);
-		return num/denom;
-	}
-	
-	public static double[] listToArray(List<Double> arr){   
-	    double[] result = new double[arr.size()];
-	    int i = 0;
-	    for(Double d : arr) {
-	        result[i++] = d.doubleValue();
-	    }
-	    return result;
-	}
-	
-	
-	//FIX ELSE STATEMENT to generalize
-	public double getMean(double[] da){
-		double m = 0;
-		for (double d: da){
-			m += d;
-		}
-		if (da.length != 0){
-			return m/da.length;
-		}
-		else{
-			return random.nextDouble()*2;
-		}
-	}
-	
-	//FIX ELSE STATEMENT to generalize
-	public double getSd(double[] da, double mean){
-		double sd = 0;
-		for (double d: da){
-			sd += Math.pow(d-mean, 2);
-		}
-		if (sd!=0){
-			return Math.sqrt(sd/da.length);
-		}
-		else{	//Quickfix to avoid 0 sd
-			return 0.01; 
-		}
-	}
-	
-	
 	public HashMap<String, Matrix> hankelSVD(HashMap<String, Double> fvals){
 		//CREATE HANKEL CODE HERE: 
 		//SingularValueDecomposition svd = hankelMatrix.svd();
@@ -253,11 +153,17 @@ public class HMM {
 		Matrix hls = new Matrix(new double[][]{ {0, 0.2, 0.14} } );
 		Matrix hpl = new Matrix(new double[][]{ {0, 0.2, 0.14} } ).transpose();
 		
-		//hls.print(5,5);
+		
+		Hbar.print(5,5);
+		/*
+		Ha.print(5,5);
+		Hb.print(5,5);
+		*/
 		
 		SingularValueDecomposition svd = Hbar.svd();
 		Matrix p = svd.getU().times( svd.getS() );
-		Matrix s = svd.getV();
+		Matrix s = svd.getV().transpose();
+		
 		Matrix pinv = p.inverse();
 		Matrix sinv = s.inverse();
 		
@@ -267,13 +173,126 @@ public class HMM {
 		Matrix alpha0 = hls.times(sinv);	//alpha0 row
 		Matrix alphainf = pinv.times(hpl);	//alphainf column
 		
+		/*
 		Aa.print(5,5);
 		Ab.print(5,5);
 		alpha0.print(5,5);
 		alphainf.print(5,5);
+		*/
 		
-		Matrix r = alpha0.times(Ab).times(alphainf);
-		r.print(5,5);
+		Matrix test1 = alpha0.times(Aa).times(alphainf);
+		Matrix test2 = alpha0.times(Ab).times(alphainf);
+		Matrix test3 = alpha0.times(Aa).times(Ab).times(alphainf);
+		Matrix test4 = alpha0.times(Ab).times(Aa).times(alphainf);
+		Matrix test5 = alpha0.times(Aa).times(Aa).times(alphainf);
+		Matrix test6 = alpha0.times(Ab).times(Ab).times(alphainf);
+		
+		test1.print(5,5);
+		test2.print(5,5);
+		test3.print(5,5);
+		test4.print(5,5);
+		test5.print(5,5);
+		test6.print(5,5);
+	}
+	
+	
+	
+	// HELPER FUNCTIONS BELOW
+	
+	public static double[] generateProbabilityVector(int length, double p){	
+		double[] result = new double[length]; 
+		for(int i=0;i<length;i++){
+			result[i] = nChooseI(length-1, i)*Math.pow(p,i)*Math.pow(1-p, length-1-i);
+		} 
+	
+		return result;
+	}
+	
+	//[0.4, 0.5, 0.1] --> returns index in {0,1,2}
+	public static int generateState(double[] stateProbabilities){ 						
+		int l = stateProbabilities.length; 
+		double[] cumulativeSum = new double[l];
+		
+		cumulativeSum[0] = stateProbabilities[0];
+		for (int i = 1; i<l;i++){
+			cumulativeSum[i] = cumulativeSum[i-1] + stateProbabilities[i];
+		}
+
+		double r = random.nextDouble();
+		
+		int index = Arrays.binarySearch(cumulativeSum, r);
+
+		if (index >= 0){
+			return index;
+		}
+		else{
+			return -1*(index + 1);
+		}
+		
+	}	
+	
+	public static double[] listToArray(List<Double> arr){   
+	    double[] result = new double[arr.size()];
+	    int i = 0;
+	    for(Double d : arr) {
+	        result[i++] = d.doubleValue();
+	    }
+	    return result;
+	}
+	
+	public static double sumArray(double[] da){
+		double s = 0;
+		for(double d: da){
+			s += d;
+		}
+		return s;
+	}
+	
+	
+	//FIX ELSE STATEMENT to generalize
+	public static double getMean(double[] da){
+		double m = 0;
+		for (double d: da){
+			m += d;
+		}
+		if (da.length != 0){
+			return m/da.length;
+		}
+		else{
+			return random.nextDouble()*2;
+		}
+	}
+	
+	//FIX ELSE STATEMENT to generalize
+	public static double getSd(double[] da, double mean){
+		double sd = 0;
+		for (double d: da){
+			sd += Math.pow(d-mean, 2);
+		}
+		if (sd!=0){
+			return Math.sqrt(sd/da.length);
+		}
+		else{	//Quickfix to avoid 0 sd
+			return 0.01; 
+		}
+	}
+	
+	public static double getLikelyhood(double modelMean, double modelSd, double obs){
+		double num = Math.pow(Math.E, -1*Math.pow(modelMean - obs,2) / (2*Math.pow(modelSd,2)) );
+		double denom = Math.sqrt(2*Math.pow(modelSd,2)*Math.PI);
+		return num/denom;
+	}
+	
+	public static int nChooseI(int n, int i){
+		return factorial(n)/ ( factorial(i)*factorial(n-i) );
+	}
+	public static int factorial(int n){
+		if (n == 0){
+			return 1;
+		}
+		else{
+			return n*factorial(n-1);
+		}
 	}
 	
 	
--- src/hmm_sim/HelperFunctions.java
+++ src/hmm_sim/HelperFunctions.java
@@ -1,7 +1,107 @@
 package hmm_sim;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class HelperFunctions {
+	
+	public static Random random = new Random();
 
 	
+	public static double[] generateProbabilityVector(int length, double p){	
+		double[] result = new double[length]; 
+		for(int i=0;i<length;i++){
+			result[i] = nChooseI(length-1, i)*Math.pow(p,i)*Math.pow(1-p, length-1-i);
+		} 
 	
+		return result;
+	}
+	
+	//[0.4, 0.5, 0.1] --> returns index in {0,1,2}
+	public static int generateState(double[] stateProbabilities){ 						
+		int l = stateProbabilities.length; 
+		double[] cumulativeSum = new double[l];
+		
+		cumulativeSum[0] = stateProbabilities[0];
+		for (int i = 1; i<l;i++){
+			cumulativeSum[i] = cumulativeSum[i-1] + stateProbabilities[i];
+		}
+
+		double r = random.nextDouble();
+		
+		int index = Arrays.binarySearch(cumulativeSum, r);
+
+		if (index >= 0){
+			return index;
+		}
+		else{
+			return -1*(index + 1);
+		}
+		
+	}	
+	
+	public static double[] listToArray(List<Double> arr){   
+	    double[] result = new double[arr.size()];
+	    int i = 0;
+	    for(Double d : arr) {
+	        result[i++] = d.doubleValue();
+	    }
+	    return result;
+	}
+	
+	public static double sumArray(double[] da){
+		double s = 0;
+		for(double d: da){
+			s += d;
+		}
+		return s;
+	}
+	
+	
+	//FIX ELSE STATEMENT to generalize
+	public static double getMean(double[] da){
+		double m = 0;
+		for (double d: da){
+			m += d;
+		}
+		if (da.length != 0){
+			return m/da.length;
+		}
+		else{
+			return random.nextDouble()*2;
+		}
+	}
+	
+	//FIX ELSE STATEMENT to generalize
+	public static double getSd(double[] da, double mean){
+		double sd = 0;
+		for (double d: da){
+			sd += Math.pow(d-mean, 2);
+		}
+		if (sd!=0){
+			return Math.sqrt(sd/da.length);
+		}
+		else{	//Quickfix to avoid 0 sd
+			return 0.01; 
+		}
+	}
+	
+	public static double getLikelyhood(double modelMean, double modelSd, double obs){
+		double num = Math.pow(Math.E, -1*Math.pow(modelMean - obs,2) / (2*Math.pow(modelSd,2)) );
+		double denom = Math.sqrt(2*Math.pow(modelSd,2)*Math.PI);
+		return num/denom;
+	}
+	
+	public static int nChooseI(int n, int i){
+		return factorial(n)/ ( factorial(i)*factorial(n-i) );
+	}
+	public static int factorial(int n){
+		if (n == 0){
+			return 1;
+		}
+		else{
+			return n*factorial(n-1);
+		}
+	}
 }
